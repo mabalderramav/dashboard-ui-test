e73632ede7bc53b004ac78fb3de0a1fead5c313f
Jimmy Romero Sejas <JimmyRomero@Fundacion-jala.local>
2016-09-23 19:17:05 -0400

    home page and menus automation

---
+++ src/main/java/org/fundacionjala/dashboard/ui/page/GroupPage.java
@@ -0,0 +1,10 @@
+package org.fundacionjala.dashboard.ui.page;
+
+import javax.swing.text.AbstractDocument;
+
+/**
+ * Created by JimmyRomero on 9/23/2016.
+ */
+public class GroupPage extends AbstractBasePage {
+
+}
