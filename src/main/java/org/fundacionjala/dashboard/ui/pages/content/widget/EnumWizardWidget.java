a1ad8104e533089d834b013b35150a75b700247b
bruno vasquez <bruno.vasquez@fundacion-jala.org>
2016-10-06 18:12:31 -0400

    Widget dinamic configurations

---
+++ src/main/java/org/fundacionjala/dashboard/ui/pages/content/widget/EnumWizardWidget.java
@@ -0,0 +1,23 @@
+package org.fundacionjala.dashboard.ui.pages.content.widget;
+
+/**
+ * Created by BrunoVasquez on 10/5/2016.
+ */
+public enum EnumWizardWidget {
+    PROJECT,
+    STORY,
+    ITERATION,
+    BURN,
+    STORY_TYPE,
+    TASK_BURN;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return name().toLowerCase();
+    }
+
+
+}
