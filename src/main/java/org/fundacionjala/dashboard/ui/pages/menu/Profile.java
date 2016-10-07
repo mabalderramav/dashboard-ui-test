4177633bf1e90e2e5a2c7f6ddf9addca016b70c7
Lourdes Villca Lazaro <LourdesVillca@Fundacion-jala.local>
2016-10-07 10:50:28 -0400

    add hook to delete all services

--- src/main/java/org/fundacionjala/dashboard/ui/pages/menu/Profile.java
+++ src/main/java/org/fundacionjala/dashboard/ui/pages/menu/Profile.java
@@ -1,5 +1,7 @@
 package org.fundacionjala.dashboard.ui.pages.menu;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,6 +15,10 @@ public class Profile extends AbstractBasePage {
 
     @FindBy(css = "button[data-action=\"add-instance\"]")
     private WebElement addServiceButton;
+
+    @FindBy(css = "div.ui.stackable.three.column.grid")
+    private WebElement serviceSection;
+
 
     /**
      * Method to perform a click to add service button.
@@ -32,5 +38,32 @@ public class Profile extends AbstractBasePage {
     public Service clickSetting() {
         driver.findElement(By.cssSelector("i.setting.link.icon")).click();
         return new Service();
+    }
+
+
+    /**
+     * Method to perform a click the delete icon.
+     */
+
+    public void clickDeleteIcon() {
+        driver.findElement(By.cssSelector("i.remove.link.icon")).click();
+    }
+
+    /**
+     * Method to perform the click in the OK button in the alert message to confirm the delete option.
+     */
+    public void clickOKAlertMessage() {
+        driver.switchTo().alert().accept();
+    }
+
+    /**
+     * This method delete all services.
+     */
+    public void deleteAllServices() {
+        List<WebElement> serviceList = serviceSection.findElements(By.className("column"));
+        for (WebElement x : serviceList) {
+            x.findElement(By.cssSelector("i.remove.link.icon")).click();
+            clickOKAlertMessage();
+        }
     }
 }
