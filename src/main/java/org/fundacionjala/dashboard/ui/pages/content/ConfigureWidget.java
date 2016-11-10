55094f9d2cf0a88ca71b4cb7c8d1edfaaacca683
Angela Valdez Bernal <AngelaValdez@Fundacion-jala.local>
2016-11-10 17:48:43 -0400

    Refactor

--- src/main/java/org/fundacionjala/dashboard/ui/pages/content/ConfigureWidget.java
+++ src/main/java/org/fundacionjala/dashboard/ui/pages/content/ConfigureWidget.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import org.fundacionjala.dashboard.ui.pages.AbstractBasePage;
